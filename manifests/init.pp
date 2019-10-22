68ed138a9d2aa8fc923e0db79567c24cab976a5f
Przemek Anuszek <przemas@anuszek.net>
2019-10-22 20:20:44 +0200

    gg

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,31 +4,29 @@
 #
 ################################################################################
 class smarthost (
-    $mta = false,
-    $smarthost = false,
-    $domain = $::fqdn,
-) {
+  $mta = false,
+  $smarthost = false,
+  $domain = $::fqdn,
+) inherits smarthost::params {
 
-    include 'smarthost::params'
-    $mta_real = $mta ? {
-        false   => $smarthost::params::mta,
-        default => $mta,
-    }
+  $mta_real = $mta ? {
+    false   => $smarthost::params::mta,
+    default => $mta,
+  }
 
-    if ( $mta_real == false ) {
-        $osver = $smarthost::params::osver
-        fail("Unsupported OS ${osver}")
-    }
+  if ( $mta_real == false ) {
+    $osver = $smarthost::params::osver
+    fail("Unsupported OS ${osver}")
+  }
+  if ( $smarthost::smarthost == false ) {
+    fail('No smarthost specified')
+  }
 
-    if ( $smarthost::smarthost == false ) {
-        fail('No smarthost specified')
-    }
-
-    case $mta_real {
-        'exim4':    { include 'smarthost::exim4'            }
-        'sendmail': { include 'smarthost::sendmail'         }
-        'postfix':  { include 'smarthost::postfix'          }
-        default:    { fail("Unsupported MTA ${mta_real}")   }
-    }
+  case $mta_real {
+    'exim4':    { include 'smarthost::exim4'            }
+    'sendmail': { include 'smarthost::sendmail'         }
+    'postfix':  { include 'smarthost::postfix'          }
+    default:    { fail("Unsupported MTA ${mta_real}")   }
+  }
 
 }
