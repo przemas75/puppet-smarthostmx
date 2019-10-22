7298753f9aa0e3fd68d32972cb5f360e9a97ed50
Przemek Anuszek <przemas@anuszek.net>
2019-10-22 10:59:17 +0200

    osver

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,6 +10,10 @@ class smarthost (
   $domain    = $::fqdn,
 ) inherits smarthost::params {
 
+  if ! $osver !=  'unset' {
+    fail("System version not set: \$osver")
+  }
+
   case $mta {
     'exim4':    { include smarthost::exim4           }
     'sendmail': { include smarthost::sendmail        }
