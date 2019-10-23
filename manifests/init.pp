578e6e4aaf587cfdabe835043669ad0a3095668a
Przemek Anuszek <przemas@anuszek.net>
2019-10-23 23:13:12 +0200

    bhy

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,72 +4,9 @@
 #
 ################################################################################
 class smarthost (
-  $mta = false,
-  $smarthost = false,
-  $domain = $::fqdn,
-) {
-  if $facts['operatingsystemmajrelease'] {
-      $osver = "${::operatingsystem}-${::operatingsystemmajrelease}"
-  } else {
-      $osver = "${::operatingsystem}-${::operatingsystemrelease}"
-  }
-
-  case $facts['operatingsystem'] {
-    'Ubuntu' : {
-      $smarthost = 'localhost'
-    }
-    'RedHat' : {
-      $smarthost = 'localhost'
-    }
-    'CentOS' : {
-      $smarthost = 'localhost'
-    }
-    'Debian' : {
-      $smarthost = undef
-    }
-    default     : {
-      fail("The ${module_name} module is not supported on an ${::osfamily} based system.")
-    }
-  }
-
-  case $osver {
-      'CentOS-5':         { $mta = 'sendmail' }
-      'CentOS-6':         { $mta = 'postfix'  }
-      'CentOS-7':         { $mta = 'postfix'  }
-      'Debian-7':         { $mta = 'exim4'    }
-      /^Debian-jessie/:   { $mta = 'exim4'    }
-      'CloudLinux-5':     { $mta = 'sendmail' }
-      'CloudLinux-6':     { $mta = 'postfix'  }
-      'CloudLinux-7':     { $mta = 'postfix'  }
-      'RedHat-5':         { $mta = 'sendmail' }
-      'RedHat-6':         { $mta = 'postfix'  }
-      'RedHat-7':         { $mta = 'postfix'  }
-      'Ubuntu-10.04':     { $mta = 'postfix'  }
-      'Ubuntu-12.04':     { $mta = 'postfix'  }
-      'Ubuntu-14.04':     { $mta = 'postfix'  }
-      default:            { $mta = false      }
-  }
-    $mta_real = $mta ? {
-    false   => $smarthost::params::mta,
-    default => $mta,
-  }
-
-
-
-
-  if ( $mta_real == false ) {
-    $osver = $smarthost::params::osver
-    fail("Unsupported OS ${osver}")
-  }
-  if ( $smarthost::smarthost == false ) {
-    fail('No smarthost specified')
-  }
-
-  case $mta_real {
-    'exim4':    { include 'smarthost::exim4'            }
-    'sendmail': { include 'smarthost::sendmail'         }
-    'postfix':  { include 'smarthost::postfix'          }
-    default:    { fail("Unsupported MTA ${mta_real} for osver ${osver}") }
-  }
+  $mta       = $smarthost::params::mta,
+  $smarthost = $smarthost::params::smarthost,
+  $domain    = $::fqdn,
+) inherits smarthost::params {
 
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -4,11 +4,61 @@
 #
 ################################################################################
 class smarthost::params {
+  $mta       = undef
+  $smarthost = undef
 
-  # Default MTA selection based on OS & Version
+  if $facts['operatingsystemmajrelease'] {
+      $osver = "${::operatingsystem}-${::operatingsystemmajrelease}"
+  } else {
+      $osver = "${::operatingsystem}-${::operatingsystemrelease}"
+  }
 
+  if ! defined($mta) {
+    case $facts['operatingsystem'] {
+      'Ubuntu' : {
+        $smarthost = 'localhost'
+      }
+      'RedHat' : {
+        $smarthost = 'localhost'
+      }
+      'CentOS' : {
+        $smarthost = 'localhost'
+      }
+      'Debian' : {
+        $smarthost = undef
+      }
+      default     : {
+        fail("The ${module_name} module is not supported on an ${::osfamily} based system.")
+      }
+    }
+  }
 
-  $mta = undef
+  if ! defined($mta) {
+    case $osver {
+        'CentOS-5':         { $mta = 'sendmail' }
+        'CentOS-6':         { $mta = 'postfix'  }
+        'CentOS-7':         { $mta = 'postfix'  }
+        'Debian-7':         { $mta = 'exim4'    }
+        /^Debian-jessie/:   { $mta = 'exim4'    }
+        'CloudLinux-5':     { $mta = 'sendmail' }
+        'CloudLinux-6':     { $mta = 'postfix'  }
+        'CloudLinux-7':     { $mta = 'postfix'  }
+        'RedHat-5':         { $mta = 'sendmail' }
+        'RedHat-6':         { $mta = 'postfix'  }
+        'RedHat-7':         { $mta = 'postfix'  }
+        'Ubuntu-10.04':     { $mta = 'postfix'  }
+        'Ubuntu-12.04':     { $mta = 'postfix'  }
+        'Ubuntu-14.04':     { $mta = 'postfix'  }
+        default:            { $mta = false      }
+    }
+  }
+
+  case $mta {
+    'exim4':    { include 'smarthost::exim4'            }
+    'sendmail': { include 'smarthost::sendmail'         }
+    'postfix':  { include 'smarthost::postfix'          }
+    default:    { fail("Unsupported MTA ${mta_real} for osver ${osver}") }
+  }
 
 
   $paths = {
