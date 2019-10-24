73a9fa34656fd0e8e148d95f71b2d9cd0ffaa353
Przemek Anuszek <przemas@anuszek.net>
2019-10-24 15:28:53 +0200

    pa

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,7 @@
 class smarthost (
   Optional[String] $mta       = $smarthost::params::mta,
   Optional[String] $smarthost = $smarthost::params::smarthost,
+  Optional[String] $paths     = $smarthost::params::paths,
 ) inherits smarthost::params {
 
 }
