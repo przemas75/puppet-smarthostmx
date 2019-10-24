de5d5f0675ecb426a1ff6a42c0d9675179d54063
Przemek Anuszek <przemas@anuszek.net>
2019-10-24 20:51:49 +0200

    d

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,5 +8,9 @@ class smarthost (
   Optional[String] $smarthost = $smarthost::params::smarthost,
   Optional[String] $paths     = $smarthost::params::paths,
 ) inherits smarthost::params {
+  contain 'smarthost::exim4'
+  contain 'smarthost::postfix'
+  contain 'smarthost::sendmail'
+
 
 }
