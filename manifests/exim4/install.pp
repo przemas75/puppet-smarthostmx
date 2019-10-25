1b46dd2a21952d6e27e1acc1a3f5142f51bf2e74
Przemek Anuszek <przemas@anuszek.net>
2019-10-25 13:47:05 +0200

    sa

--- manifests/exim4/install.pp
+++ manifests/exim4/install.pp
@@ -5,7 +5,7 @@
 ################################################################################
 class smarthost::exim4::install {
 
-    $paths      = $smarthost::params::paths
+    $paths      = $smarthost::paths
     $packages   = $smarthost::exim4::packages
     $services   = $smarthost::exim4::services
     $smarthost  = $smarthost::smarthost
--- manifests/init.pp
+++ manifests/init.pp
@@ -7,10 +7,15 @@ class smarthost (
   Optional[String] $mta       = $smarthost::params::mta,
   Optional[String] $smarthost = $smarthost::params::smarthost,
   Optional[String] $paths     = $smarthost::params::paths,
+  $osfamily                   = $::osfamily,
 ) inherits smarthost::params {
+
   contain 'smarthost::exim4'
   contain 'smarthost::postfix'
   contain 'smarthost::sendmail'
 
-
+  $paths = {
+      make                => '/usr/bin/make',
+      update_exim4_conf   => '/usr/sbin/update-exim4.conf',
+  }
 }
