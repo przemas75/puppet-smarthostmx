09411b864c54d97c6143953772912ad1302f705b
Przemek Anuszek <przemas@anuszek.net>
2019-10-26 09:29:38 +0200

    e

--- manifests/exim4/install.pp
+++ manifests/exim4/install.pp
@@ -5,10 +5,10 @@
 ################################################################################
 class smarthost::exim4::install {
 
-    $paths      = $smarthost::paths
+    # $paths      = $smarthost::paths
     $packages   = $smarthost::exim4::packages
     $services   = $smarthost::exim4::services
-    $smarthost  = $smarthost::smarthost
+    # $smarthost  = $smarthost::smarthost
 
     package { $packages :
         ensure  => present,
